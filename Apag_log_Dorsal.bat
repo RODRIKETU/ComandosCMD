72a71ae9f62a3d59ee73af5924604ac007f7d8a2
Rodrigo Augusto <RODRIKETU@ME.COM>
2025-06-26 15:16:52 -0300

    Adiçao de Funções

---
+++ Apag_log_Dorsal.bat
@@ -0,0 +1,79 @@
+
+cls
+
+cd\
+
+cd\dorsal
+
+del LogBanco*.*
+del LogBalanca_2015*.*
+del LogBalanca_2016*.*
+del LogBalanca_2017*.*
+del LogBalanca_2018*.*
+del LogBalanca_2019*.*
+del LogBalanca_2020*.*
+del LogBalanca_2021*.*
+del LogBalanca_2022*.*
+del LogBalanca_2023*.*
+del LogBalanca_2024*.*
+del LogBalanca_2025*.*
+
+del LogECF_2015*.*
+del LogECF_2016*.*
+del LogECF_2017*.*
+del LogECF_2018*.*
+del LogECF_2019*.*
+del LogECF_2020*.*
+del LogECF_2021*.*
+del LogECF_2022*.*
+del LogECF_2023*.*
+del LogECF_2024*.*
+del LogECF_2025*.*
+
+del LogSerial_2015*.*
+del LogSerial_2016*.*
+del LogSerial_2017*.*
+del LogSerial_2018*.*
+del LogSerial_2019*.*
+del LogSerial_2020*.*
+del LogSerial_2021*.*
+del LogSerial_2022*.*
+del LogSerial_2023*.*
+del LogSerial_2024*.*
+del LogSerial_2025*.*
+
+
+cd nfce
+del LogNFCe_2015*.*
+del LogNFCe_2016*.*
+del LogNFCe_2017*.*
+del LogNFCe_2018*.*
+del LogNFCe_2019*.*
+del LogNFCe_2020*.*
+del LogNFCe_2021*.*
+del LogNFCe_2022*.*
+del LogNFCe_2023*.*
+del LogNFCe_2024*.*
+del LogNFCe_2025*.*
+
+
+cd..
+cd log
+del *.* /q
+
+cd..
+cd web
+del *.* /q
+
+cd log
+del DorsalWeb_2015*.*
+del DorsalWeb_2016*.*
+del DorsalWeb_2017*.*
+del DorsalWeb_2018*.*
+del DorsalWeb_2019*.*
+del DorsalWeb_2020*.*
+del DorsalWeb_2021*.*
+del DorsalWeb_2022*.*
+del DorsalWeb_2023*.*
+del DorsalWeb_2024*.*
+del DorsalWeb_2025*.*
